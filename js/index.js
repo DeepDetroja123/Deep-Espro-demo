d39bc38b08b618336221a70050093e526a3acc8c
DeepDetroja123 <deep@praella.com>
2023-08-24 19:02:38 +0530

    make nav in mobile menu and make hover effect

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 function toggleNav() {
     var hemburgmenu = document.getElementById("menu-icon-mobile");
     var closemenu = document.getElementById("cancel-icon");
+    var mobnav = document.getElementById("mobile-nav-link");
     if (hemburgmenu.classList.contains('d-block')) {
         hemburgmenu.classList.remove('d-block')
         hemburgmenu.classList.add('d-none');
         closemenu.classList.remove('d-none');
         closemenu.classList.add('d-block');
+        mobnav.style.left = "0%";
     } else {
         hemburgmenu.classList.remove('d-none');
         hemburgmenu.classList.add('d-block');
         closemenu.classList.remove('d-block');
         closemenu.classList.add('d-none');
+        mobnav.style.left = "-100%";
     }
-
 }
 
 
