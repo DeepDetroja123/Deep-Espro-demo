12f4a145a8cd916b62b5f7ece50aa6fb44f00181
DeepDetroja123 <deep@praella.com>
2023-08-24 10:59:18 +0530

    close and open icon set in mob-menu

---
+++ js/index.js
@@ -0,0 +1,22 @@
+function toggleNav() {
+    var hemburgmenu = document.getElementById("menu-icon-mobile");
+    var closemenu = document.getElementById("cancel-icon");
+    if (hemburgmenu.classList.contains('d-block')) {
+        hemburgmenu.classList.remove('d-block')
+        hemburgmenu.classList.add('d-none');
+        closemenu.classList.remove('d-none');
+        closemenu.classList.add('d-block');
+    } else {
+        hemburgmenu.classList.remove('d-none');
+        hemburgmenu.classList.add('d-block');
+        closemenu.classList.remove('d-block');
+        closemenu.classList.add('d-none');
+    }
+
+}
+
+
+
+
+
+
