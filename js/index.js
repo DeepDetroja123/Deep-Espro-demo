8265c8e68efb0d2326d73d31278ef233665016fa
DeepDetroja123 <deep@praella.com>
2023-09-06 18:54:45 +0530

    Fix Bug in HomePage during QA and cont with it

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 function toggleNav() {
     var hemburgmenu = document.getElementById("menu-icon-mobile");
     var closemenu = document.getElementById("cancel-icon");
-    var mobnav = document.getElementById("mobile-nav-link");
+    var mobnav = document.getElementById("mobileNavLink");
     if (hemburgmenu.classList.contains('d-block')) {
         hemburgmenu.classList.remove('d-block')
         hemburgmenu.classList.add('d-none');
         closemenu.classList.remove('d-none');
         closemenu.classList.add('d-block');
-        mobnav.style.left = "0";
+        mobnav.style.left = '0';
         document.body.style.overflow = "hidden";
     } else {
         hemburgmenu.classList.remove('d-none');
         hemburgmenu.classList.add('d-block');
         closemenu.classList.remove('d-block');
         closemenu.classList.add('d-none');
-        mobnav.style.left = "-100%";
+        mobnav.style.left = '-100%';
         document.body.style.overflow = "visible";
     }
 }
